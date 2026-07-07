9ddc8bd1da7f2f669c88cee1ce12392684a91533
Diana <ddmitrieva098@example.com>
2026-07-07 18:10:10 +1100

    добавлены пагинаторы

--- tracker/views.py
+++ tracker/views.py
@@ -2,11 +2,13 @@ from rest_framework import viewsets, generics
 
 from tracker.models import PleasantHabit, UsefulHabit
 from tracker.serializers import PleasantHabitSerializer, UsefulHabitSerializer
+from tracker.paginators import HabitPagination
 
 
 class PleasantHabitViewSet(viewsets.ModelViewSet):
     serializer_class = PleasantHabitSerializer
     queryset = PleasantHabit.objects.all()
+    pagination_class = HabitPagination
 
     def perform_create(self, serializer):
         pleasant_habit = serializer.save(user=self.request.user)
@@ -24,6 +26,7 @@ class UsefulHabitCreateView(generics.CreateAPIView):
 class UsefulHabitListView(generics.ListAPIView):
     queryset = UsefulHabit.objects.all()
     serializer_class = UsefulHabitSerializer
+    pagination_class = HabitPagination
 
 
 class UsefulHabitDetailView(generics.RetrieveAPIView):
